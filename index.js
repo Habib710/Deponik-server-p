9c930f428e8e156f1d8dbba49042d51c52dc6208
habib <mdhabib71024@gmail.com>
2022-05-02 13:08:09 +0600

    single data load done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').conflig;
 const port=process.env.PORT || 5000 ;
 const app=express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middelware
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://Deponic:HmpspJu0CcrrnDcR@cluster0.qb6yh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qb6yh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
@@ -27,6 +27,14 @@ async function run(){
         const cursor = Itemscollection.find(query);
         const Allitems= await cursor.toArray();
         res.send(Allitems);
+      })
+      app.get('/items/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:ObjectId(id)};
+        const item=await Itemscollection.findOne(query);
+        res.send(item);
+
+
       })
 
 
