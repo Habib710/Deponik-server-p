7e8fbe22dedf0ad49092ce43d77e6270213b21aa
habib <mdhabib71024@gmail.com>
2022-05-06 12:43:41 +0600

    prb

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const port=process.env.PORT || 5000 ;
 const app=express();
 require('dotenv').config();
-const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
